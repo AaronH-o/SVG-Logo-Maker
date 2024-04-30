ec7f9cec02e46e31c27e9abfdaa4d48bd3472090
Aaron Ho <bicecreamaar@gmail.com>
2024-04-30 00:33:01 -0700

    added inquirer to the project

--- index.js
+++ index.js
@@ -1,2 +1,52 @@
 const inquirer = require('inquirer');
 const fs = require('node:fs');
+
+const questions = [
+  'Enter text (up to 3 characters',
+  'Enter a color for the text',
+  'Choose a shape',
+  'Enter a color for the shape'
+];
+
+function init() {
+  let data = {
+    text: '',
+    textColor: '',
+    shape: '',
+    shapeColor: '',
+  };
+
+  inquirer
+    .prompt([
+      {
+        name: "text",
+        type: "input",
+        message: questions[0],
+      },
+      {
+        name: "textColor",
+        type: "input",
+        message: questions[1],
+      },
+      {
+        name: "shape",
+        type: "list",
+        message: questions[2],
+        choices: ["Circle", "Triangle", "Square"],
+      },
+      {
+        name: "shapeColor",
+        type: "input",
+        message: questions[3],
+      },
+    ])
+    .then((answer) => {
+      data.text = answer.text;
+      data.textColor = answer.textColor;
+      data.shape = answer.shape;
+      data.shapeColor = answer.shapeColor;
+
+
+
+    })
+}
